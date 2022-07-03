98c1cb2a44f1f73db8f1c6f9411f6865d7f978ec
arturhbs <arturhenrique71@gmail.com>
2022-07-03 16:40:50 -0300

    Criacao do Bot para tabelar dados atraves de uma URL

--- bot.py
+++ bot.py
@@ -17,34 +17,43 @@ class Bot:
         # tranforma cada campo em que é uma lista ou dicionario para que seja criado um parquet para cada um
         df_notas = data.pop('Notas', None)
         df_notas = pd.DataFrame(df_notas, columns=['Notas'])
-        self._df_to_parquet(df_notas, 'df_notas')
+        self._df_to_parquet(df_notas, 'tb_notas')
 
         df_unidades_medida = data.pop('UnidadesDeMedida', None)
         df_unidades_medida = pd.DataFrame(df_unidades_medida)
-        self._df_to_parquet(df_unidades_medida, 'df_unidades_medida')
+        self._df_to_parquet(df_unidades_medida, 'tb_unidades_medida')
 
         df_variaveis = data.pop('Variaveis', None)
         df_variaveis = pd.DataFrame(df_variaveis)
-        self._df_to_parquet(df_variaveis, 'df_variaveis')
+        self._df_to_parquet(df_variaveis, 'tb_variaveis')
 
         df_classificacoes = data.pop('Classificacoes', None)
         df_classificacoes = self._df_normalize_json(df_classificacoes)
-        self._df_to_parquet(df_classificacoes, 'df_classificacoes')
+        self._df_to_parquet(df_classificacoes, 'tb_classificacoes')
 
         df_pesquisa_aux = data.pop('Pesquisa', None)
-        df_pesquisa = self._df_normalize_json(df_pesquisa_aux)
-        self._df_to_parquet(df_pesquisa, 'df_pesquisa')
+        df_pesquisa = self._df_normalize_json(df_pesquisa_aux['Temas'])
+        df_pesquisa['Id'] = df_pesquisa_aux['Id']
+        df_pesquisa['Nome'] = df_pesquisa_aux['Nome']
+        self._df_to_parquet(df_pesquisa, 'tb_pesquisa')
 
         df_periodos_aux = data.pop('Periodos', None)
-        df_periodos = self._df_normalize_json(df_periodos_aux)
-        self._df_to_parquet(df_periodos, 'df_periodos')
+        df_periodos = self._df_normalize_json(df_periodos_aux['Periodos'])
+        df_periodos['Nome'] = df_periodos_aux['Nome']
+        df_periodos['Periodicidade'] = df_periodos_aux['Periodicidade']
+        self._df_to_parquet(df_periodos, 'tb_periodos_periodos')
+
+        df_periodos_conjuntos = self._df_normalize_json(df_periodos_aux['Conjuntos'])
+        df_periodos_conjuntos = df_periodos_conjuntos.explode(column='Periodos')
+        self._df_to_parquet(df_periodos_conjuntos, 'tb_periodos_conjuntos')
+
 
         df_territorios_aux = data.pop('Territorios', None)
         df_territorios = self._df_normalize_json(df_territorios_aux)
-        self._df_to_parquet(df_territorios, 'df_territorios')
+        self._df_to_parquet(df_territorios, 'tb_territorios')
 
         df_data = pd.json_normalize(data)
-        self._df_to_parquet(df_data, 'df_data')
+        self._df_to_parquet(df_data, 'tb_data')
 
     @staticmethod
     def _df_normalize_json(df):
