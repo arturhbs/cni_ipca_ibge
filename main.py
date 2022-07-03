e24ff1d07b3949fa5fd556e2f404eb8ad7d8537b
arturhbs <arturhenrique71@gmail.com>
2022-07-03 16:41:34 -0300

    Criacao do Bot para tabelar dados atraves de uma URL

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from bot import Bot
 
 # Inicializar com a URL pedida no roteiro
 url = 'https://sidra.ibge.gov.br/Ajax/JSon/Tabela/1/1737?versao=-1'
-# Chamando a classe Bot do arquivo bot.py
+# Chamando a classe Bot
 Bot(url).main()
 
